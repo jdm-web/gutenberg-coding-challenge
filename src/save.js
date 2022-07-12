844f53470101fa257a967394491f73bf30463c06
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2022-07-12 20:08:48 +0200

    refacto(save): Add missing useBlockProps.

--- src/save.js
+++ src/save.js
@@ -1,11 +1,16 @@
 /**
  * Internal dependencies
  */
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
 import Preview from './preview';
 
 export default function Save( { attributes } ) {
+	const blockProps = useBlockProps.save();
 	return (
-		<div>
+		<div { ...blockProps }>
 			<Preview { ...attributes } />
 		</div>
 	);
